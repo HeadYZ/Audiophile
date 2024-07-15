1e0c406a5371fed45faa2d2037b455827c2faaa3
Patryk <krasowski.p@outlook.com>
2024-07-15 21:21:28 +0200

    styles tablet header, start styling desktop header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,24 @@
 import { Outlet } from 'react-router-dom'
 import Button from '../UI/Button'
 import classes from './HomePage.module.scss'
+// import heroImage from '../assets/home/desktop/image-hero.jpg'
 export default function HomePage() {
 	return (
 		<>
 			<Outlet />
 			<header className={classes.header}>
 				<div className={classes.header__wrapper}>
-					<span className={classes.header__heading}>New product</span>
-					<h1 className={classes.header__product}>XX99 mark II headphones</h1>
-					<p className={classes['header__text-body']}>
-						Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
-					</p>
-					<Button btn='1'>See product</Button>
+					<article>
+						<span className={classes.header__heading}>New product</span>
+						<h1 className={classes.header__product}>XX99 mark II headphones</h1>
+						<p className={classes['header__text-body']}>
+							Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
+						</p>
+						<Button btn='1'>See product</Button>
+					</article>
+					<div className={classes.header__hero}>
+						{/* <img src={heroImage} alt='XX99 mark II headphones' className={classes['header__hero-img']} /> */}
+					</div>
 				</div>
 			</header>
 		</>
