b76b24a9c533b12b165887550497e3566877f742
Patryk <krasowski.p@outlook.com>
2024-07-16 20:36:18 +0200

    update header styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Button from '../UI/Button'
 import classes from './HomePage.module.scss'
-// import heroImage from '../assets/home/desktop/image-hero.jpg'
 export default function HomePage() {
 	return (
 		<>
 			<Outlet />
 			<header className={classes.header}>
 				<div className={classes.header__wrapper}>
-					<article>
+					<article className={classes.header__box}>
 						<span className={classes.header__heading}>New product</span>
 						<h1 className={classes.header__product}>XX99 mark II headphones</h1>
 						<p className={classes['header__text-body']}>
@@ -16,9 +15,6 @@ export default function HomePage() {
 						</p>
 						<Button btn='1'>See product</Button>
 					</article>
-					<div className={classes.header__hero}>
-						{/* <img src={heroImage} alt='XX99 mark II headphones' className={classes['header__hero-img']} /> */}
-					</div>
 				</div>
 			</header>
 		</>
