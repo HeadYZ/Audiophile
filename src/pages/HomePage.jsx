a4105a3f19824015ddd84a347cc3f37d304d7366
Patryk <krasowski.p@outlook.com>
2024-07-17 18:46:50 +0200

    styling headers item

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
 import { Link, Outlet } from 'react-router-dom'
 import Button from '../UI/Button'
 import classes from './HomePage.module.scss'
+import earphonesImg from '../assets/home/shared/desktop/image-category-thumbnail-earphones.png'
+import speakersImg from '../assets/home/shared/desktop/image-category-thumbnail-speakers.png'
+import headphonesImg from '../assets/home/shared/desktop/image-category-thumbnail-headphones.png'
+import ArrowRightIcon from '../assets/home/shared/desktop/ArrowRight.svg'
 export default function HomePage() {
 	return (
 		<>
@@ -21,19 +25,57 @@ export default function HomePage() {
 				<section className={classes.products}>
 					<ul className={classes.products__list}>
 						<li className={classes['products__list-item']}>
-							<img src='' alt='' />
-							<h3>Headphones</h3>
-							<Link>Shop</Link>
+							<div className={classes['products__list-item-top']}>
+								<img
+									src={headphonesImg}
+									alt=''
+									className={`${classes['products__list-img ']} ${classes['products__list-img--1']}`}
+								/>
+							</div>
+
+							<div className={classes['products__list-box']}>
+								<h3 className={classes['products__list-item-heading']}>Headphones</h3>
+								<Link className={classes['products__list-item-link']}>
+									<span>Shop</span>
+									<img src={ArrowRightIcon} alt='' />
+								</Link>
+							</div>
 						</li>
 						<li className={classes['products__list-item']}>
-							<img src='' alt='' />
-							<h3>Speakers</h3>
-							<Link>Shop</Link>
+							<div className={classes['products__list-item-top']}>
+								<img
+									src={speakersImg}
+									alt=''
+									className={`${classes['products__list-img ']} ${classes['products__list-img--2']}`}
+								/>
+							</div>
+
+							<div className={classes['products__list-box']}>
+								<h3 className={classes['products__list-item-heading']}>Speakers</h3>
+								<Link className={classes['products__list-item-link']}>
+									{' '}
+									<span>Shop</span>
+									<img src={ArrowRightIcon} alt='' />
+								</Link>
+							</div>
 						</li>
 						<li className={classes['products__list-item']}>
-							<img src='' alt='' />
-							<h3>Earphones</h3>
-							<Link>Shop</Link>
+							<div className={classes['products__list-item-top']}>
+								<img
+									src={earphonesImg}
+									alt=''
+									className={`${classes['products__list-img ']} ${classes['products__list-img--3']}`}
+								/>
+							</div>
+
+							<div className={classes['products__list-box']}>
+								<h3 className={classes['products__list-item-heading']}>Earphones</h3>
+								<Link className={classes['products__list-item-link']}>
+									{' '}
+									<span>Shop</span>
+									<img src={ArrowRightIcon} alt='' />
+								</Link>
+							</div>
 						</li>
 					</ul>
 				</section>
