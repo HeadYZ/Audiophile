c014f81d9332e795686528fba6ae111ef04cab42
Patryk <krasowski.p@outlook.com>
2024-07-25 21:15:06 +0200

    styled tablet & desktop info section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -129,14 +129,27 @@ export default function HomePage() {
 				</section>
 				<section className={classes.info}>
 					<div className={classes.info__wrapper}>
-						<div>
-							<picture>
-								<source media='(min-width: 768px) and (max-width: 967px )' srcSet={manTabletImg} />
-								<source media='(min-width: 968px)' srcSet={manDesktopImg} />
-								<img src={manMobileImg} alt='Man wearing earphones listening to music' />
-							</picture>
+						<picture className={classes.info__picture}>
+							<source media='(min-width: 768px) and (max-width: 992px )' srcSet={manTabletImg} />
+							<source media='(min-width: 992px)' srcSet={manDesktopImg} />
+							<img
+								src={manMobileImg}
+								alt='Man wearing earphones listening to music'
+								className={classes['info__picture-img']}
+							/>
+						</picture>
+
+						<div className={classes.info__box}>
+							<h2 className={classes.info__heading}>
+								Bringing you the <span>best</span> audio gear
+							</h2>
+							<p className={classes.info__text}>
+								Located at the heart of New York City, Audiophile is the premier store for high end headphones,
+								earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms
+								available for you to browse and experience a wide range of our products. Stop by our store to meet some
+								of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
+							</p>
 						</div>
-						<div></div>
 					</div>
 				</section>
 			</main>
