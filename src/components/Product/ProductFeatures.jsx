2237202a020a54378ac66779630909351fe4928b
Patryk <krasowski.p@outlook.com>
2024-08-15 21:06:42 +0200

    styling product features

--- src/components/Product/ProductFeatures.jsx
+++ src/components/Product/ProductFeatures.jsx
@@ -4,15 +4,17 @@ import classes from './ProductFeatures.module.scss'
 export default function ProductFeatures({ features, box }) {
 	return (
 		<article className={classes.product__features}>
-			<h2 className={classes['product__features-heading']}>Features</h2>
+			<h3 className={`${classes['product__features-heading']} ${classes['product__features-heading--first']}`}>
+				Features
+			</h3>
 			<p className={classes['product__features-text']}>{features}</p>
 			<div className={classes['product__features-box']}>
-				<h2 className={classes['product__features-box-heading']}>In the box</h2>
+				<h3 className={classes['product__features-heading']}>In the box</h3>
 				<ul className={classes['product__features-list']}>
 					{box.map(product => (
 						<li key={product.item} className={classes['product__features-list-item']}>
-							<span className={classes['product__features-list-quantity']}>{product.quantity}</span>
-							{product.item}
+							<span className={classes['product__features-list-quantity']}>{product.quantity}x</span>
+							<span className={classes['product__features-list-text']}>{product.item}</span>
 						</li>
 					))}
 				</ul>
--- src/components/Product/ProductInfo.jsx
+++ src/components/Product/ProductInfo.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
+import { useRef } from 'react'
 import Button from '../../UI/Button.jsx'
 import classes from './ProductInfo.module.scss'
 export default function ProductInfo({ name, price, images, isNew, category, description }) {
 	const productTitle = name.split(' ').slice(0, -1).join(' ')
 	const formatedPrice = new Intl.NumberFormat().format(price)
+	const productQuantity = useRef()
+
 	return (
 		<div className={classes.product__box}>
 			<div className={classes.product__picture}>
@@ -23,7 +26,28 @@ export default function ProductInfo({ name, price, images, isNew, category, desc
 				<p className={classes.product__description}>{description}</p>
 				<span className={classes.product__price}>$ {formatedPrice}</span>
 				<div className={classes['product__box-second']}>
-					<input type='number' defaultValue='1' className={classes['product__box-second-input']} />
+					<div className={classes['product__box-second-tools']}>
+						<button
+							onClick={() => productQuantity.current.stepDown()}
+							className={classes['product__box-second-decrement']}
+						>
+							-
+						</button>
+						<input
+							type='number'
+							ref={productQuantity}
+							defaultValue='1'
+							min='1'
+							step='1'
+							className={classes['product__box-second-input']}
+						/>
+						<button
+							onClick={() => productQuantity.current.stepUp()}
+							className={classes['product__box-second-increment']}
+						>
+							+
+						</button>
+					</div>
 					<Button btn='1'>Add to cart</Button>
 				</div>
 			</article>
