6b7b802f56bf837bd93876148f54f94fe913afb3
Patryk <krasowski.p@outlook.com>
2024-08-16 20:57:02 +0200

    styling mobile productadditionalingo

--- src/components/Product/ProductAdditionInfo.jsx
+++ src/components/Product/ProductAdditionInfo.jsx
@@ -9,12 +9,12 @@ export default function ProductAdditionInfo({ products, images }) {
 	return (
 		<>
 			<div className={classes.product__images}>
-				<div className={classes['product__images-box']}>
+				<div className={`${classes['product__images-box']} ${classes['product__images-box--first']}`}>
 					{gallery.map((image, id) => (
 						<picture key={id * Math.random() + 'image'}>
 							<source media='(min-width: 768px) and (max-width: 1440px )' srcSet={image.tablet} />
 							<source media='(min-width: 1440px)' srcSet={image.desktop} />
-							<img src={image.mobile} />
+							<img src={image.mobile} className={classes['product__images-picture']} />
 						</picture>
 					))}
 				</div>
@@ -22,24 +22,26 @@ export default function ProductAdditionInfo({ products, images }) {
 					<picture>
 						<source media='(min-width: 768px) and (max-width: 1440px )' srcSet={lastImage[0].tablet} />
 						<source media='(min-width: 1440px)' srcSet={lastImage[0].desktop} />
-						<img src={lastImage[0].mobile} />
+						<img src={lastImage[0].mobile} className={classes['product__images-picture']} />
 					</picture>
 				</div>
 			</div>
 			<div>
-				<h3>You may also like</h3>
-				<ul>
+				<h3 className={classes['product__addition-heading']}>You may also like</h3>
+				<ul className={classes['product__addition-list']}>
 					{products.map(product => (
-						<li key={product.slug}>
-							<div>
+						<li key={product.slug} className={classes['product__addition-list-item']}>
+							<div className={classes['product__addition-picture']}>
 								<picture>
 									<source media='(min-width: 768px) and (max-width: 1440px )' srcSet={product.image.tablet} />
 									<source media='(min-width: 1440px)' srcSet={product.image.desktop} />
-									<img src={product.image.mobile} alt={product.name} />
+									<img src={product.image.mobile} alt={product.name} className={classes['product__addition-img']} />
 								</picture>
 							</div>
-							<h4>{product.name}</h4>
-							<Button btn='1'>See product</Button>
+							<div className={classes['product__addition-box']}>
+								<h4 className={classes['product__addition-box-heading']}>{product.name}</h4>
+								<Button btn='1'>See product</Button>
+							</div>
 						</li>
 					))}
 				</ul>
